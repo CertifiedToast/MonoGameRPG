725160fcb1ed51f8462a9328c5b1e97c8dc19bb6
CertifiedToast <72758872+CertifiedToast@users.noreply.github.com>
2021-05-22 11:50:03 +0100

    Add project files.

---
+++ MonoGameRPG/Manager/Page.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace MonoGameRPG.Manager
+{
+    public abstract class Page
+    {
+        public int id;
+
+        public Page(int id)
+        {
+            this.id = id;
+        }
+
+        public abstract void Init(RPG_Game g);
+
+        public abstract void Update(GameTime gt, RPG_Game g);
+
+        public abstract void Draw(RPG_Game g);
+    }
+}
---
+++ MonoGameRPG/Manager/PageManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace MonoGameRPG.Manager
+{
+    public class PageManager
+    {
+        public List<Page> pages = new List<Page>();
+        public int count { get { return pages.Count; } }
+        public int selectedPage;
+    }
+}
