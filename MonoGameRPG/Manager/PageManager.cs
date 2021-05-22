8b187d4fb6854814b02cac8e5b544b84ca11b45a
CertifiedToast <72758872+CertifiedToast@users.noreply.github.com>
2021-05-22 12:09:07 +0100

    Added a page manager to assist with change of scenes

--- MonoGameRPG/Manager/PageManager.cs
+++ MonoGameRPG/Manager/PageManager.cs
@@ -9,5 +9,25 @@ namespace MonoGameRPG.Manager
         public List<Page> pages = new List<Page>();
         public int count { get { return pages.Count; } }
         public int selectedPage;
+
+        public void Update(GameTime gt, RPG_Game g) 
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Page page = pages[i];
+                if (page.id == selectedPage) { page.Update(gt, g); }
+            }
+        }
+        public void Draw(RPG_Game g)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Page page = pages[i];
+                if (page.id == selectedPage) { page.Draw(g); }
+            }
+        }
+        public void Add(Page page, RPG_Game g) { pages.Add(page); page.Init(g); }
+        public void Remove(Page page) { pages.Remove(page); }
+        public void Clear() { pages.Clear(); }
     }
 }
